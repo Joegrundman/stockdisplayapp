95bc72d18370b74e1536c489437127e4e3e9ff99
Joe Grundman <Joe Grundman>
2016-11-25 13:27:11 +0100

    materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { MaterializeDirective } from 'angular2-materialize'
 
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component'
@@ -14,7 +15,8 @@ import { StockDataService } from './stock-service/stock.service'
     AppComponent,
     ChartComponent,
     SearchbarComponent,
-    StocksComponent
+    StocksComponent,
+    MaterializeDirective
   ],
   imports: [
     BrowserModule,
