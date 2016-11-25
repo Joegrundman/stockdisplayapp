5df7b365e5c93a575c90fcf8785711a888be68c4
Joe Grundman <Joe Grundman>
2016-11-25 21:23:04 +0100

    remove materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
-import { MaterializeDirective } from 'angular2-materialize'
 
 import { AppComponent } from './app.component';
 import { ChartComponent } from './chart/chart.component'
@@ -15,8 +14,7 @@ import { StockDataService } from './stock-service/stock.service'
     AppComponent,
     ChartComponent,
     SearchbarComponent,
-    StocksComponent,
-    MaterializeDirective
+    StocksComponent
   ],
   imports: [
     BrowserModule,
