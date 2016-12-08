955ede1721268aec97eb8c535386d26700fd3246
Joe Grundman <Joe Grundman>
2016-12-08 11:33:41 +0100

    socket

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'dist')))
+
+// app.listen(app.get('port'), function() {
+//     console.log('Express server listening on port ' + app.get('port'))
+// })
+
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.on('connection', function(socket){
+    onlineUsers++;
+
+    io.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+    socket.on('disconnect', function() {
+        onlineUsers--;
+        io.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+    });
+});
+
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'))
+})
