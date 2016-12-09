7b9716899a4a23759a46b77228447171bc071052
Joe Grundman <Joe Grundman>
2016-12-09 18:16:26 +0100

    ystocks api used in server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 var path = require('path')
 var logger = require('morgan')
 var bodyParser = require('body-parser')
+var ystocks = require('ystocks')(/* params for api key or public */)
 
 var app = express()
 
@@ -13,17 +14,91 @@ app.use(express.static(path.join(__dirname, 'dist')))
 
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
+
 var onlineUsers = 0;
+var activeStocks = ['YHOO', 'MSFT', 'FB'];
+var stockData = {};
+var completedPromises = 0;
+var dates;
+var monthsToSearch = 3
 
-var activeStocks = ['YHOO', 'MSFT', 'FB']
+var errorMessage = "No results!"
 
-io.on('connection', function(socket){
+function getDates(months) {
+    var now = new Date();
+    var endYear = now.getFullYear();
+    var endMonth = now.getMonth() + 1;
+    var date = now.getDate();
+    var startMonth = endMonth - months;
+    var startYear = endYear;
+    if (startMonth < 1) {
+        startMonth += 12;
+        startYear -= 1;
+    }
+
+    var startDate = startYear + '-' + startMonth + '-' + date;
+    var endDate = endYear + '-' + endMonth + '-' + date;
+
+    return [startDate, endDate];
+}
+
+dates = getDates(monthsToSearch);
+
+function getActiveStockData(socketEmitCallback) {
+
+    activeStocks.forEach(symbol => {
+
+        if (stockData[symbol] && stockData[symbol] != errorMessage) {
+            completedPromises ++
+            return
+        }
+
+        var params = {
+            symbol: symbol,
+            start: dates[0],
+            end: dates[1]
+        };
+
+        ystocks.history(params, function (err, data, meta) {
+            if (err) {
+            } else if (data) {
+                var useData = data.map(d => {
+                    return {
+                        Symbol: d.Symbol,
+                        Close: d.Close,
+                        Date: d.Date
+                    }
+                })
+                stockData[symbol] = useData;
+                if (socketEmitCallback) { 
+                    socketEmitCallback() 
+                }
+                completedPromises++;
+            } else {
+                stockData[symbol] = { msg: errorMessage };
+                completedPromises++;
+            }
+        })
+    })
+
+}
+
+getActiveStockData();
+
+
+io.on('connection', function (socket) {
     onlineUsers++;
 
     io.emit('onlineUsers', { onlineUsers: onlineUsers });
     io.emit('activeStocksUpdate', { activeStocks: activeStocks });
 
-    socket.on('disconnect', function() {
+    if (completedPromises == activeStocks.length) {
+        io.emit('stockData', { stockData: stockData });
+        completedPromises = 0;
+    }
+
+    socket.on('disconnect', function () {
         onlineUsers--;
         io.emit('onlineUsers', { onlineUsers: onlineUsers });
 
@@ -33,17 +108,23 @@ io.on('connection', function(socket){
         io.emit('activeStocksUpdate', { activeStocks: activeStocks });
     });
 
-    socket.on('deleteStock', function(toDelete) {
+    socket.on('deleteStock', function (toDelete) {
         activeStocks = activeStocks.filter(stock => stock != toDelete)
+        delete stockData[toDelete]
         io.emit('activeStocksUpdate', { activeStocks: activeStocks });
+        io.emit('stockData', { stockData: stockData})
     })
 
-    socket.on('addStock', function(toAdd) {
+    socket.on('addStock', function (toAdd) {
+        console.log('addstock', toAdd)
         activeStocks = activeStocks.concat([toAdd])
-        io.emit('activeStocksUpdate', { activeStocks: activeStocks });        
+        io.emit('activeStocksUpdate', { activeStocks: activeStocks });
+        var socketEmitCallback = () =>  io.emit('stockData', { stockData: stockData });
+        getActiveStockData(socketEmitCallback)
+
     })
 });
 
-server.listen(app.get('port'), function() {
+server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'))
 })
